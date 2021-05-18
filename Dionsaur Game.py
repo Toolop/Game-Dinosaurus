10a04663b66e677e87e5bdcfd40cce6e707ba0bb
Toolop <toolop2312@gmail.com>
2021-05-18 12:08:13 +0700

    Memperbaiki pause dan tampilan

--- Dionsaur Game.py
+++ Dionsaur Game.py
@@ -1,50 +1,59 @@
 import pygame
-import os 
+import os
 import random
 
 pygame.init()
 
-pygame.display.set_caption("First Game")
+pygame.display.set_caption("Dino Game")
 
 screen_H = 700
 screen_W = 500
 
-#assets
-canvas = pygame.display.set_mode((screen_H,screen_W))
+# assets
+canvas = pygame.display.set_mode((screen_H, screen_W))
 
-RUNNING = [pygame.image.load(os.path.join("Assets\Dino", "DinoRun1.png")),
-           pygame.image.load(os.path.join("Assets\Dino", "DinoRun2.png"))]
+RUNNING = [pygame.image.load(os.path.join("Assets\Dino", "1.png")),
+           pygame.image.load(os.path.join("Assets\Dino", "2.png")),
+           pygame.image.load(os.path.join("Assets\Dino", "3.png"))]
 
 DUCK = [pygame.image.load(os.path.join("Assets\Dino", "DinoDuck1.png")),
-       pygame.image.load(os.path.join("Assets\Dino", "DinoDuck2.png"))]
+        pygame.image.load(os.path.join("Assets\Dino", "DinoDuck2.png"))]
 
 CLOUD = pygame.image.load(os.path.join("Assets\Other", "Cloud.png"))
 
 OBSTACLE_SMALL = [pygame.image.load(os.path.join("Assets\Cactus", "SmallCactus1.png")),
-                  pygame.image.load(os.path.join("Assets\Cactus", "SmallCactus2.png")),
+                  pygame.image.load(os.path.join(
+                      "Assets\Cactus", "SmallCactus2.png")),
                   pygame.image.load(os.path.join("Assets\Cactus", "SmallCactus3.png"))]
 
 OBSTACLE_LARGE = [pygame.image.load(os.path.join("Assets\Cactus", "LargeCactus1.png")),
                   pygame.image.load(os.path.join("Assets\Cactus", "LargeCactus2.png")),
                   pygame.image.load(os.path.join("Assets\Cactus", "LargeCactus3.png"))]
 
+OBSTACLE_BIRD = [pygame.image.load(os.path.join("Assets\Bird", "Bird1.png")),
+                 pygame.image.load(os.path.join("Assets\Bird", "Bird2.png"))]
+
 ROAD = pygame.image.load(os.path.join("Assets\Other", "Track.png"))
 
-START = pygame.image.load(os.path.join("Assets\Other","Start.png"))
-EXIT = pygame.image.load(os.path.join("Assets\Other","exit.png"))
-RESET = pygame.image.load(os.path.join("Assets\Other","Reset.png"))
-GAME_OVER = pygame.image.load(os.path.join("Assets\Other","GameOver.png"))
+START = pygame.image.load(os.path.join("Assets\Other", "Start.png"))
+EXIT = pygame.image.load(os.path.join("Assets\Other", "exit.png"))
+EXIT_btn_restart = pygame.image.load(os.path.join("Assets\Other", "exitbtn.png"))
+RESET = pygame.image.load(os.path.join("Assets\Other", "Reset.png"))
+GAME_OVER = pygame.image.load(os.path.join("Assets\Other", "GameOver.png"))
+
+white = (255,255,255)
+black = (0,0,0)
 
 class player:
     pos_x = 15
     pos_y = 330
 
     def __init__(self):
-        #image
+        # image
         self.run_img = RUNNING
         self.duck_img = DUCK
 
-        self.jump = False 
+        self.jump = False
         self.run = True
         self.duck = False
 
@@ -65,33 +74,33 @@ class player:
             self.jump_act()
         if self.duck:
             self.duck_act()
-            
+
         if self.step_index >= 10:
             self.step_index = 0
 
-        if keys[pygame.K_SPACE] and not self.jump :
+        if keys[pygame.K_SPACE] and not self.jump:
             self.run = False
             self.jump = True
-            self.duck =  False
-        elif keys[pygame.K_DOWN] and not self.jump :
+            self.duck = False
+        elif keys[pygame.K_DOWN] and not self.jump:
             self.run = False
             self.jump = False
             self.duck = True
-        elif not self.jump or self.duck :
+        elif not self.jump or self.duck:
             self.run = True
             self.jump = False
             self.duck = False
 
     def run_act(self):
         global game_speed
-        self.image = self.run_img[self.step_index  // 5]
+        self.image = self.run_img[self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = player.pos_x
         self.dino_rect.y = player.pos_y
         self.step_index += int(game_speed/10)
-        
+
     def jump_act(self):
-        if self.jump: 
+        if self.jump:
             self.dino_rect.y -= self.jump_vel * 5
             self.jump_vel -= 0.6
         if self.jump_vel < - self.do_jump:
@@ -100,11 +109,11 @@ class player:
 
     def duck_act(self):
         global game_speed
-        self.image = self.duck_img[self.step_index //5]
+        self.image = self.duck_img[self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = player.pos_x
         self.dino_rect.y = player.pos_y + self.do_duck
-        self.step_index +=  int(game_speed/10)
+        self.step_index += int(game_speed/10)
 
     def draw(self, canvas):
         canvas.blit(self.image, self.dino_rect)
@@ -113,56 +122,60 @@ class score:
     def __init__(self):
         self.points = -1
         self.count = 0
-        self.font = pygame.font.Font('freesansbold.ttf',20)
+        self.font = pygame.font.Font('freesansbold.ttf', 20)
 
     def nambah(self):
         if(self.count % 5 == 0 and self.count > 1):
             self.points += 11
-        else :
-            self.points +=1 
+        else:
+            self.points += 1
         if (self.count % 10 == 0 and self.count >= 1):
             global game_speed
             game_speed += 1
-        self.count +=1      
-    def tampilkan(self):
-        
-        text = self.font.render("Points : " + str(self.points),True,(0,0,0))
-        textRect = text.get_rect() 
-        textRect.center = (100 ,40)
-        canvas.blit(text,textRect)      
+        self.count += 1
+
+    def tampilkan(self,malam=False):
+        if (malam):
+            text = self.font.render("Points : " + str(self.points), True, white)
+        else : 
+            text = self.font.render("Points : " + str(self.points), True, black)
+        textRect = text.get_rect()
+        textRect.center = (100, 40)
+        canvas.blit(text, textRect)
 
 class cloud:
     def __init__(self):
-        self.x = screen_H + random.randint(500,800)
-        self.y = random.randint(50,100)
+        self.x = screen_H + random.randint(500, 800)
+        self.y = random.randint(50, 100)
         self.image = CLOUD
         self.width = self.image.get_width()
-    
+
     def update(self):
         self.x -= game_speed
         if self.x < - self.width:
-            self.x = screen_W + random.randint(2500,3000)
-            self.y = random.randint(50,100)
+            self.x = screen_W + random.randint(2500, 3000)
+            self.y = random.randint(50, 100)
 
     def draw(self, canvas):
-        canvas.blit(self.image, (self.x,self.y))
+        canvas.blit(self.image, (self.x, self.y))
 
 class bg:
     bg_x = 0
     bg_y = 400
+
     def __init__(self):
         self.image_width = ROAD.get_width()
 
     def update(self):
-        canvas.blit(ROAD, (bg.bg_x,bg.bg_y))
+        canvas.blit(ROAD, (bg.bg_x, bg.bg_y))
         canvas.blit(ROAD, (self.image_width + bg.bg_x, bg.bg_y))
         if bg.bg_x <= -self.image_width:
-            canvas.blit(ROAD, (self.image_width + bg.bg_x,bg.bg_y))
+            canvas.blit(ROAD, (self.image_width + bg.bg_x, bg.bg_y))
             bg.bg_x = 0
         bg.bg_x -= game_speed
 
 class obstacle:
-    def __init__(self,image,tipe):
+    def __init__(self, image, tipe):
         self.image = image
         self.type = tipe
         self.rect = self.image[self.type].get_rect()
@@ -174,51 +187,75 @@ class obstacle:
             rintangan.pop()
 
     def draw(self, canvas):
-        canvas.blit(self.image[self.type],self.rect)
+        canvas.blit(self.image[self.type], self.rect)
 
 class smallobstacle(obstacle):
-    def __init__(self,image):
-        self.type = random.randint(0,2)
-        super().__init__(image,self.type)
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
         self.rect.y = 350
 
 class Largeobstacle(obstacle):
-    def __init__(self,image):
-        self.type = random.randint(0,2)
-        super().__init__(image,self.type)
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
         self.rect.y = 325
 
-def start() : 
-    run = True 
-    
+class Bird(obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 270
+        self.index = 0
+
+    def draw(self, canvas):
+        if self.index >= 9:
+            self.index = 0
+        canvas.blit(self.image[self.index//5], self.rect)
+        self.index += 1
+
+def start():
+    run = True
+
     trex = player()
     Cloud = cloud()
     Background = bg()
     skor = score()
-    
-    global game_speed,rintangan
+
+    global game_speed, rintangan
     game_speed = 10
     rintangan = []
 
-    while run :
+    while run:
         clock = pygame.time.Clock()
- 
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    pause()
+
         keys = pygame.key.get_pressed()
 
-        canvas.fill((255,255,255))
+        if (skor.points // 10 % 2 == 1):
+            canvas.fill(black)
+            skor.tampilkan(True)
+        else : 
+            canvas.fill(white)
+            skor.tampilkan()
         trex.draw(canvas)
         trex.update(keys)
 
         if len(rintangan) == 0:
-            if random.randint(0,1) == 1 :
+            if random.randint(0, 1) == 1:
                 rintangan.append(smallobstacle(OBSTACLE_SMALL))
                 skor.nambah()
-            elif random.randint(0,1) == 1:
+            elif random.randint(0, 1) == 1:
                 rintangan.append(Largeobstacle(OBSTACLE_LARGE))
+                skor.nambah()
+            elif random.randint(0, 1) == 1:
+                rintangan.append(Bird(OBSTACLE_BIRD))
                 skor.nambah()
 
         for i in rintangan:
@@ -226,20 +263,49 @@ def start() :
             i.update()
             if trex.dino_rect.colliderect(i.rect):
                 restart(skor.points)
-        
+
         Background.update()
         Cloud.draw(canvas)
         Cloud.update()
-        skor.tampilkan()
 
         clock.tick(30)
         pygame.display.update()
 
     pygame.quit()
 
+def pause():
+    paused = True
+    while paused:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    paused = False
+
+                elif event.key == pygame.K_q:
+                    pygame.quit()
+                    quit()
+                
+                elif event.key == pygame.K_r:
+                    paused = False
+                    start()
+
+        canvas.fill((255, 255, 255))
+        font = pygame.font.Font('freesansbold.ttf', 20)
+        text = font.render(
+            "Press C to continue or Q to quit or R to new game", True, (0, 0, 0))
+        textRect = text.get_rect()
+        textRect.center = (350, 150)
+        canvas.blit(text, textRect)
+        pygame.display.update()
+        clock = pygame.time.Clock()
+        clock.tick(5)
+
 def restart(skor):
     run = True
-    while run :
+    while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -249,35 +315,36 @@ def restart(skor):
                 elif exit_rect.collidepoint(event.pos):
                     run = False
 
-        canvas.fill((255,255,255))
+        canvas.fill((255, 255, 255))
 
-        font = pygame.font.Font('freesansbold.ttf',20)
-        if (skor < 5):
-            text = font.render("Kamu Cupu poin kamu cuma : " + str(skor),True,(0,0,0))
-        else :
-            text = font.render("Kamu hebat banget poin kamu : " + str(skor),True,(0,0,0))
-        textRect = text.get_rect() 
-        textRect.center = (350 , 150)
-        canvas.blit(text,textRect) 
+        font = pygame.font.Font('freesansbold.ttf', 20)
+        if (skor < 10):
+            text = font.render("Kamu Cupu poin kamu cuma : " + str(skor), True, (0, 0, 0))
+        else:
+            text = font.render("Kamu hebat banget poin kamu : " + str(skor), True, (0, 0, 0))
+        
+        textRect = text.get_rect()
+        textRect.center = (350, 150)
+        canvas.blit(text, textRect)
 
         restart_rect = RESET.get_rect()
-        restart_rect.x = 300
+        restart_rect.x = 270
         restart_rect.y = 270
 
-        exit_rect = EXIT.get_rect()
-        exit_rect.x = 270
-        exit_rect.y = 400
-        canvas.blit(EXIT,exit_rect)
+        exit_rect = EXIT_btn_restart.get_rect()
+        exit_rect.x = 370
+        exit_rect.y = 275
+        canvas.blit(EXIT_btn_restart, exit_rect)
 
-        canvas.blit(GAME_OVER,(170,200))
-        canvas.blit(RESET,restart_rect)
+        canvas.blit(GAME_OVER, (170, 200))
+        canvas.blit(RESET, restart_rect)
         pygame.display.update()
 
-    pygame.quit() 
+    pygame.quit()
 
 def main_menu():
-    run = True 
-    while run :
+    run = True
+    while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -287,7 +354,7 @@ def main_menu():
                 elif exit_rect.collidepoint(event.pos):
                     run = False
 
-        canvas.fill((255,255,255))
+        canvas.fill((255, 255, 255))
 
         start_rect = START.get_rect()
         start_rect.x = 265
@@ -297,12 +364,11 @@ def main_menu():
         exit_rect.x = 265
         exit_rect.y = 250
 
-        canvas.blit(START,start_rect)
-        canvas.blit(EXIT,exit_rect)
+        canvas.blit(START, start_rect)
+        canvas.blit(EXIT, exit_rect)
         pygame.display.update()
 
     pygame.quit()
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main_menu()
-d
